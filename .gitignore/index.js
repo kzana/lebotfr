ce81c1e44e5264476e225a753d8f6864d9157e51
kzana <zana.kevin@gmail.com>
2019-01-16 16:39:42 +0100

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,142 @@
+const discord = require('discord.js');
+
+const client =  new discord.Client();
+
+const prefix = "?";
+
+const { Client, Attachment } = require('discord.js');
+
+client.on('ready', function() {
+    console.log("I'm ready !");
+});
+
+client.on('message', msg => {
+    const guildTag = msg.channel.type === 'text' ? `[${msg.guild.name}]` : '[DM]';
+    const channelTag = msg.channel.type === 'text' ? `[#${msg.channel.name}]` : '';
+    console.log(`${guildTag}${channelTag} ${msg.author.tag}: ${msg.content}`);
+  });
+
+
+client.login('NTM0ODE0Mzk2MTc0NTY1Mzc2.Dx_ESw.KYfdBEBrAL_GOQsiM8hk5LV1xNM')
+.then(result=>{
+    console.log("Connexion reussie 1", result);   
+}).catch(error => {
+    console.log("Erreur lors du login... Message :",error);    
+});
+
+client.on('message', message => {
+    if (message.content === 'what is my avatar') {
+        message.reply(message.author.avatarURL);
+    }
+});
+
+client.on('message', message => {
+    if (message.content === '!rip') {
+               message.channel.send(attachment);
+    }
+});
+
+
+client.on('message', message => {
+    if(message.content === "?help") {
+        message.channel.send({embed: {
+            color: 3447003,
+            title: 'Help !',
+            url: 'https://www.google.com',
+            description: `
+**what is my avatar**
+-pour que le bot vous envoie votre avatar url.
+
+**!rip**
+-pour que le bot envoie un message ***RIP***.
+
+__Pour le staff__
+
+**?ban + mentions**
+-pour que le bot ban le joueur.
+
+**?mute + mentions** 
+-pour que le bot mute le joueur
+
+**?clear + 1-100**
+-pour que le bot clear des messages.
+
+**?kick + mentions**
+-pour que le bot kick le joueur.
+
+~~Pour pouvoir utilisé les commandes du staff, ils vous faut quelque perms~~`
+        }});
+        
+    }
+});
+
+/*Kick*/
+client.on('message',message =>{
+    if (!message.guild) return
+    let args = message.content.trim().split(/ +/g)
+
+    if (args[0].toLocaleLowerCase() === prefix + 'kick'){
+        if (!message.member.hasPermission('KICK_MEMBERS')) return message.channel.send("Vous n'avez pas la permission d'utiliser cette commande :rage:")
+        let member = message.mentions.members.first()
+        if (!member) return message.channel.send("Veuillez mentionner un utilisateur :x:")
+        if (member.highestRole.calculatedPosition >= message.member.highestRole.calculatedPosition && message.author.id !== message.guild.owner.id) return message.channel.send("Vous ne pouvez pas kick cet utilisateur :x:")
+        if (!member.kickable) return message.channel.send("Je ne peux pas exclure cet utilisateur, il doit être très fort :sunglasses:")
+        member.kick()
+        message.channel.send("**"+member.user.username + '** a été exclu :white_check_mark:')
+    }
+});
+
+/*Ban*/
+client.on('message',message =>{
+    if (!message.guild) return
+    let args = message.content.trim().split(/ +/g)
+ 
+    if (args[0].toLocaleLowerCase() === prefix + 'ban'){
+       if (!message.member.hasPermission('BAN_MEMBERS')) return message.channel.send("Vous n'avez pas la permission d'utiliser cette commande :rage:")
+       let member = message.mentions.members.first()
+       if (!member) return message.channel.send("Veuillez mentionner un utilisateur :x:")
+       if (member.highestRole.calculatedPosition >= message.member.highestRole.calculatedPosition && message.author.id !== message.guild.owner.id) return message.channel.send("Vous ne pouvez pas bannir cet utilisateur :x:")
+       if (!member.bannable) return message.channel.send("Je ne peux pas bannir cet utilisateur, il doit être très fort :sunglasses:")
+       message.guild.ban(member, {days: 7})
+       message.channel.send("**"+member.user.username + '** a été banni :white_check_mark:')
+    }
+});
+
+/*Clear*/
+client.on("message", message => {
+    if (!message.guild) return
+    let args = message.content.trim().split(/ +/g)
+ 
+    if (args[0].toLowerCase() === prefix + "clear") {
+        if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send("Vous n'avez pas la permission d'utiliser cette commande")
+        let count = args[1]
+        if (!count) return message.channel.send("Veuillez indiquer un nombre de messages à supprimer")
+        if (isNaN(count)) return message.channel.send("Veuillez indiquer un nombre valide")
+        if (count < 1 || count > 100) return message.channel.send("Veuillez indiquer un nombre entre 1 et 100")
+        message.channel.bulkDelete(parseInt(count) + 1)
+    }
+/*Mute*/
+if (args[0].toLowerCase() === prefix + "mute") {
+    if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send("Vous n'avez pas la permission d'utiliser cette commande")
+    let member = message.mentions.members.first()
+    if (!member) return message.channel.send("Membre introuvable")
+    if (member.highestRole.calculatedPosition >= message.member.highestRole.calculatedPosition && message.author.id !== message.guild.ownerID) return message.channel.send("Vous ne pouvez pas mute ce membre")
+    if (member.highestRole.calculatedPosition >= message.guild.me.highestRole.calculatedPosition || member.id === message.guild.ownerID) return message.channel.send("Je ne peux pas mute ce membre")
+    let muterole = message.guild.roles.find(role => role.name === 'Muted')
+    if (muterole) {
+        member.addRole(muterole)
+        message.channel.send(member + ' a été mute :white_check_mark:')
+    }
+    else {
+        message.guild.createRole({name: 'Muted', permissions: 0}).then((role) => {
+            message.guild.channels.filter(channel => channel.type === 'text').forEach(channel => {
+                channel.overwritePermissions(role, {
+                    SEND_MESSAGES: false
+                })
+            })
+            member.addRole(role)
+            message.channel.send(member + ' a été mute :white_check_mark:')
+        })
+    }
+}
+});  
